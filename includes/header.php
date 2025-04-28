985ebfb66f932880f54ca0f70dbe858475227daa
Aidancodergamz <aidanorourke@hotmail.co.uk>
2025-04-28 17:57:39 +0100

    Changed the /Cub-Scouts/ in the header and htaccess files to reflect the repository name in github.

--- includes/header.php
+++ includes/header.php
@@ -3,7 +3,7 @@
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
-    define('ROOT_DIR', '/scouts/');
+    define('ROOT_DIR', '/Cub-Scouts/');
 ?>
 
 <!DOCTYPE html>
