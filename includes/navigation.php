d204ca1c1ad4e3d2a641209ba978be95e9532f32
Aidan O'Rourke <aidanorourke@hotmail.co.uk>
2025-05-05 16:39:15 +0100

    Added some extra pages for user specific experience.

--- includes/navigation.php
+++ includes/navigation.php
@@ -11,7 +11,7 @@
         </div>
 
         <ul class="nav-links" id="navLinks">
-            <?php if (!isset($_SESSION['loggedin'])): ?>
+            <?php if (!isset($_SESSION['user_type'])): ?>
                 <!-- Not logged in -->
                 <li><a href="<?= ROOT_DIR ?>">Home</a></li>
                 <li><a href="<?= ROOT_DIR ?>about">About</a></li>
@@ -21,24 +21,26 @@
                 <li><a href="<?= ROOT_DIR ?>register">Register</a></li>
 
             <?php else: ?>
-                <!-- Logged in users -->
-                <li><a href="<?= ROOT_DIR ?>">Dashboard</a></li>
                 
                 <?php if ($_SESSION['user_type'] === 'admin'): ?>
+                    <li><a href="<?= ROOT_DIR ?>admin_dash">Admin Dash</a></li>
+                    <li><a href="<?= ROOT_DIR ?>gallery">Gallery</a></li>
                     <li><a href="<?= ROOT_DIR ?>admin/users">Manage Users</a></li>
                     <li><a href="<?= ROOT_DIR ?>admin/settings">Site Settings</a></li>
 
                 <?php elseif ($_SESSION['user_type'] === 'parent'): ?>
-                    <li><a href="<?= ROOT_DIR ?>parent/schedule">My Child's Schedule</a></li>
+                    <li><a href="<?= ROOT_DIR ?>parent_dash">Parent Dash</a></li>
+                    <li><a href="<?= ROOT_DIR ?>gallery">Gallery</a></li>
                     <li><a href="<?= ROOT_DIR ?>parent/messages">Messages</a></li>
 
                 <?php elseif ($_SESSION['user_type'] === 'scout'): ?>
-                    <li><a href="<?= ROOT_DIR ?>scout/activities">Activities</a></li>
-                    <li><a href="<?= ROOT_DIR ?>scout/badges">My Badges</a></li>
+                    <li><a href="<?= ROOT_DIR ?>scout_dash">Scout Dash</a></li>
+                    <li><a href="<?= ROOT_DIR ?>games">Games</a></li>
+                    <li><a href="<?= ROOT_DIR ?>gallery">Gallery</a></li>
+                    <li><a href="<?= ROOT_DIR ?>badges">My Badges</a></li>
                 <?php endif; ?>
 
                 <!-- Common links for all logged-in users -->
-                <li><a href="<?= ROOT_DIR ?>profile">Profile</a></li>
                 <li><a href="<?= ROOT_DIR ?>handlers/logout.php">Logout</a></li>
             <?php endif; ?>
         </ul>
--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 $url = isset($_GET['url']) ? rtrim($_GET['url'], '/') : '';
 
 $routes = [
+    // All general routing is put here
     '' => 'public/index.php', 
-    'logout' => 'handlers/logout.php',
     'gallery' => 'public/gallery.php',
     'about' => 'public/about.php',
     'contact' => 'public/contact.php',
@@ -12,6 +12,20 @@ $routes = [
     'success' => 'messages/register-success.php',
     'contact_message' => 'messages/contact-message.php',
     '404' => '404.php',
+
+    //Handler pages are here
+    'logout' => 'handlers/logout.php',
+
+    // I've put all admin related pages here
+    'admin_dash' => 'public/admin/dashboard.php',
+
+    //All the parent related pages are here
+    'parent_dash' => 'public/parent/dashboard.php',
+
+    //All Scout related pages are here
+    'scout_dash' => 'public/scout/dashboard.php',
+    'badges' => 'public/scout/badges.php',
+    'games' => 'public/scout/games.php',
 ];
 
 if (array_key_exists($url, $routes)) {
